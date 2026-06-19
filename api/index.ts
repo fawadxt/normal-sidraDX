99b1f5f5ccb95809b432584d2134ebe473fa720c
fawadxt <fawadxt@users.noreply.github.com>
2026-06-19 10:51:55 +0500

    Fix Vercel API serverless handler

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
+import type { IncomingMessage, ServerResponse } from 'node:http'
 import app from '../server/app.js'
 
-export default app
+export const config = {
+  maxDuration: 60,
+}
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  return app(req, res)
+}
