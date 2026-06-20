71cdfbb254c72be1aefe31d148d621dbd5728862
fawadxt <fawadxt@users.noreply.github.com>
2026-06-20 07:40:14 +0500

    Switch SafePal connect icon to official blue brand logo

--- src/components/icons/SafePalIcon.tsx
+++ src/components/icons/SafePalIcon.tsx
@@ -2,11 +2,11 @@ type Props = {
   className?: string
 }
 
-/** Official SafePal extension icon from safepal.com brand assets */
+/** Official SafePal blue brand mark (#4A21EF) from safepal.com logo-dark.svg */
 export function SafePalIcon({ className = 'w-7 h-7' }: Props) {
   return (
     <img
-      src="/safepal-icon.svg"
+      src="/safepal-blue-logo.svg"
       alt=""
       className={`object-contain ${className}`}
       aria-hidden="true"
